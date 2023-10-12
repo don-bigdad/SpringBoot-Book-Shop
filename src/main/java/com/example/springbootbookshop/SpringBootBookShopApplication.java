d2237bcc0293e5f423a5543034c7cc044dfd86ee
Bogdan <97698639+don-bigdad@users.noreply.github.com>
2023-10-12 19:18:25 +0300

    init commit

---
+++ src/main/java/com/example/springbootbookshop/SpringBootBookShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootbookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBookShopApplication.class, args);
+    }
+
+}
