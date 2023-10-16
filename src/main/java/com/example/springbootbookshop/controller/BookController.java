e705a531f654ba3b02bcaffa48e30dc492dfbc48
Bogdan <97698639+don-bigdad@users.noreply.github.com>
2023-10-16 23:51:34 +0300

    just add response status

--- src/main/java/com/example/springbootbookshop/controller/BookController.java
+++ src/main/java/com/example/springbootbookshop/controller/BookController.java
@@ -6,12 +6,14 @@ import com.example.springbootbookshop.entity.Book;
 import com.example.springbootbookshop.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -26,6 +28,7 @@ public class BookController {
     }
 
     @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteBookById(@PathVariable Long id) {
         bookService.deleteById(id);
     }
@@ -36,6 +39,7 @@ public class BookController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Book createBook(@RequestBody CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
     }
