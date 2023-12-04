cf56bf7f13dbf0780afb46ad5d0337e822efb37f
Bogdan <97698639+don-bigdad@users.noreply.github.com>
2023-12-04 11:00:50 +0200

    some fixes,add pageable to order

---
+++ src/test/java/com/example/springbootbookshop/repository/CartRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.example.springbootbookshop.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class CartRepositoryTest {
+    @Autowired
+    private CartRepository cartRepository;
+    @Autowired
+    private BookRepository bookRepository;
+
+
+}
---
+++ src/test/resources/database/users-carts/insert-user-cart.sql
@@ -0,0 +1,5 @@
+INSERT INTO carts(id, user_id, is_deleted)
+VALUES
+    (1,1),
+    (2,2),
+    (3,3);
